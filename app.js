c1e959788c06b1c7bc3e2e2babb3d753df50791b
BakaFT <bakaft@outlook.com>
2023-08-08 15:28:29 +0800

    Fix doc

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { hookXhr} from './util'
 
-
 export function init(context) {
     context.rcp.postInit('rcp-fe-lol-l10n', api => {
         hookXhr()
--- hooks/xhr_hooks.js
+++ hooks/xhr_hooks.js
@@ -5,7 +5,7 @@ const REWARD = [
         postSend: (response) => {
             // Honor level 3 checkpoint 2
             // with 4396 times RECENT_HEART
-            // and a reward HONOR_LEVEL_5_CAPSULE,honor lv3 reward
+            // and a reward HONOR_LEVEL_5_CAPSULE, which is not honor lv3 reward
             response.response = "{\"actionType\":\"CHECKPOINT_REACHED\",\"currentState\":{\"checkpoint\":2,\"level\":3,\"rewardsLocked\":false},\"dynamicHonorMessage\":{\"messageId\":\"RECENT_HEART\",\"value\":4396},\"previousState\":{\"checkpoint\":1,\"level\":3,\"rewardsLocked\":false},\"reward\":{\"quantity\":1,\"rewardType\":\"HONOR_LEVEL_5_CAPSULE\"}}"
         }
     }
@@ -16,16 +16,17 @@ const PROMOTION = [
         matcher:'/lol-ranked/v1/notifications',
         preSend: (XhrRequestConfig) => {},
         postSend: (response) => {
-            // PROMOTE TO `NO RANKED LEVEL` lmao, please show me how it looks like in en lang
+            // PROMOTE TO `NO RANKED LEVEL` when u have no rank level  lmao
             // response.response = '[{"displayType":"VIGNETTE","msgId":"11223344","notifyReason":"LEAGUE_PROMOTED","queueType":"RANKED_SOLO_5x5"}]'
 
             // PROMOTE TO CHANLLENGER
-            response.response = '[{"tier":"CHALLENGER","displayType":"VIGNETTE","msgId":"12322224","notifyReason":"LEAGUE_PROMOTED","queueType":"RANKED_SOLO_5x5"}]'
+            response.response = '[{"tier":"MASTER","displayType":"VIGNETTE","msgId":"12322224","notifyReason":"LEAGUE_PROMOTED","queueType":"RANKED_SOLO_5x5"}]'
         }
     }
 ]
 
 // YOU MUST EXPORT WHAT U WANT TO USE HERE using `...VAR_NAME`
+// PROMOTION AND REWARD CAN BE USED TOGETHER
 export const XHR_HOOKS = [
     ...PROMOTION
 ]
